828527298ca2b3fb7f8d0d202e4bdcfe0ba2a4ce
mark wu <saybyebyet@gmail.com>
2024-12-01 22:04:47 +0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,36 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { s -> s.split("   ").mapIndexed { index, v -> index to v } }.flatten()
+            .groupBy { it.first }.values.map { it.map { it.second }.sorted() }
+            .map { it.mapIndexed { i, v -> i to v } }
+            .flatten()
+            .groupBy { it.first }
+            .values
+            .sumOf { abs(it[0].second.toInt() - it[1].second.toInt()) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val cache = HashMap<String, Int>()
+        val groups =
+            input.map { s -> s.split("   ").mapIndexed { index, v -> index to v } }.flatten()
+                .groupBy { it.first }.values.map { it.map { it.second } }
+        return groups[0].sumOf { g ->
+            val v = cache[g]
+            if (v != null) {
+                v
+            } else {
+                val count = g.toInt() * groups[1].count { it == g }
+                cache[g] = count
+                count
+            }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
