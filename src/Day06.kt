ab962143dcae929ae9e5fe67d383470cf5f5d2a1
Mark Wu <saybyebyet@gmail.com>
2024-12-09 10:04:06 +0800

    Refactor

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,41 @@
 import kotlin.math.abs
 import kotlin.math.sign
 
+enum class Dir {
+    UP, RIGHT, DOWN, LEFT;
+
+    companion object {
+        val symbols = Dir.entries.map { it.symbol }
+    }
+
+    val value: Pair<Int, Int> get() {
+        return when(this) {
+                UP -> 0 to -1 // upward
+                RIGHT -> 1 to 0 // right
+                DOWN -> 0 to 1 // downward
+                LEFT -> -1 to 0 // left
+        }
+    }
+
+    val symbol: Char get() {
+        return when(this) {
+            RIGHT -> '→'
+            LEFT -> '←'
+            UP -> '↑'
+            DOWN -> '↓'
+        }
+    }
+
+    fun rotated(): Dir {
+        return when(this) {
+            UP -> RIGHT
+            RIGHT -> DOWN
+            DOWN -> LEFT
+            LEFT -> UP
+        }
+    }
+}
+
 fun findObstacles(input: List<String>): List<Pair<Int, Int>> {
     return input.mapIndexed { i, s ->
         s.mapIndexed { j, v ->
@@ -40,35 +75,22 @@ fun getGuardMap(input: List<String>): List<List<Char>> {
     val obstacles = findObstacles(input)
     val origin = findOrigin(input)
     var start = origin
-    val dirs = listOf(
-        0 to -1, // upward
-        1 to 0, // right
-        0 to 1, // downward
-        -1 to 0 // left
-    )
     val mutableMap = input.map { it.toMutableList() }.toMutableList()
-    var dirIndex = 0
-    var (dx, dy) = dirs[dirIndex]
+    var dir = Dir.UP
+    var (dx, dy) = dir.value
     do {
         val (x, y) = start
         mutableMap[y][x] = when (mutableMap[y][x]) {
             '^' -> '^'
-            'X' -> 'Y'
-            else -> when(dirs[dirIndex]) {
-                1 to 0 -> '→'
-                -1 to 0 -> '←'
-                0 to -1 -> '↑'
-                0 to 1 -> '↓'
-                else -> '*'
-            }
+            else -> dir.symbol
         }
         val obstacle = obstacles.firstOrNull { (px, py) ->
             px == x + dx && py == y + dy
         }
         if (obstacle != null) {
-            dirIndex = (dirIndex + 1) % dirs.size
-            dx = dirs[dirIndex].first
-            dy = dirs[dirIndex].second
+            dir = dir.rotated()
+            dx = dir.value.first
+            dy = dir.value.second
         }
         start = x + dx to y + dy
     } while (start.first >= 0 && start.second >= 0 && start.first < input[0].length && start.second < input.size)
@@ -77,67 +99,59 @@ fun getGuardMap(input: List<String>): List<List<Char>> {
 
 fun canMakeALoop(obstacles: List<Pair<Int, Int>>,
                  virtualObstacle: Pair<Int, Int>,
-                 dirs: List<Pair<Int, Int>>, fromDirIndex: Int, from: Pair<Int, Int>): List<Pair<Int,Int>>{
-    val visited = HashMap<Int, HashSet<Pair<Int, Int>>>().also {
-        for (i in dirs.indices) {
+                 dir: Dir,from: Pair<Int, Int>): List<Pair<Int,Int>>{
+    val visited = HashMap<Dir, HashSet<Pair<Int, Int>>>().also {
+        for (i in Dir.entries) {
             it.putIfAbsent(i, HashSet())
         }
     }
-    visited[fromDirIndex]!!.add(virtualObstacle)
+    visited[dir]!!.add(virtualObstacle)
     var start = from
     val obstaclesOptions = obstacles + virtualObstacle
-    var currentDirIndex = (fromDirIndex + 1) % dirs.size
-    var dir = dirs[currentDirIndex]
+    var currentDir = dir.rotated()
     val selectedObstacles = ArrayList<Pair<Int, Int>>()
     selectedObstacles.add(virtualObstacle)
     while (true) {
         val ob = obstaclesOptions.filter {
-            it.isOnThePath(start, dir)
+            it.isOnThePath(start, currentDir.value)
         }.minByOrNull { it.distanceBetween(start) } ?: return emptyList()
-        if (visited[currentDirIndex]!!.contains(ob)) {
+        if (visited[currentDir]!!.contains(ob)) {
             println("$ob $selectedObstacles")
             return selectedObstacles.toList()
         }
-        start = ob.minus(dir)
+        start = ob.minus(currentDir.value)
         selectedObstacles.add(ob)
-        visited[currentDirIndex]!!.add(ob)
-        currentDirIndex = (currentDirIndex + 1) % dirs.size
-        dir = dirs[currentDirIndex]
+        visited[currentDir]!!.add(ob)
+        currentDir = currentDir.rotated()
     }
 }
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return getGuardMap(input).sumOf { it.count { s -> s == 'X' || s == 'Y' || s == '^' } }
+        return getGuardMap(input).also { println(it.joinToString("\n") ) }.sumOf { it.count { s -> Dir.symbols.contains(s) || s == '^' } }
     }
 
     fun part2(input: List<String>): Int {
         val origin = findOrigin(input)
         val obstacles = findObstacles(input)
         val guardMap = getGuardMap(input).map { it.toMutableList() }.toMutableList()
-        val dirs = listOf(
-            0 to -1, // upward
-            1 to 0, // right
-            0 to 1, // downward
-            -1 to 0 // left
-        )
-        var dirIndex = 0
         var start = origin
-        var (dx, dy) = dirs[dirIndex]
+        var dir = Dir.UP
         var count = 0
+        var (dx, dy) = dir.value
         do {
             val (x, y) = start
             val obstacle = obstacles.firstOrNull{ (px, py) ->
                 px == x + dx && py == y + dy
             }
             if (obstacle != null) {
-                dirIndex = (dirIndex + 1) % dirs.size
-                dx = dirs[dirIndex].first
-                dy = dirs[dirIndex].second
+                dir = dir.rotated()
+                dx = dir.value.first
+                dy = dir.value.second
             } else {
                 val virtualObstacle = x + dx to y + dy
                 if (virtualObstacle.first in input[0].indices && virtualObstacle.second in input.indices) {
-                    val hits = canMakeALoop(obstacles, virtualObstacle, dirs, dirIndex, x to y)
+                    val hits = canMakeALoop(obstacles, virtualObstacle, dir, x to y)
                     if (hits.isNotEmpty()) {
                         val tmpMap = input.map { it.map { it.toString() }.toMutableList() }.toMutableList()
                         for(i in hits.indices) {
