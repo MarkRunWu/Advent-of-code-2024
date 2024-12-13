944273e4befed00eb0a4b2d116ffdbf1f0ea71b4
Mark Wu <saybyebyet@gmail.com>
2024-12-13 10:21:53 +0800

    Day 12 - side

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,3 +1,5 @@
+import kotlin.math.sign
+
 enum class FarmDir {
 
     UP, DOWN, LEFT, RIGHT;
@@ -11,6 +13,15 @@ enum class FarmDir {
                 RIGHT -> 0 to 1
             }
         }
+
+    companion object {
+        fun from(edge: Pair<Pair<Int, Int>, Pair<Int, Int>>): FarmDir {
+            val (x1, y1) = edge.first
+            val (x2, y2) = edge.second
+            val dir = (x2 - x1).sign to (y2 - y1).sign
+            return entries.first { it.vec == dir }
+        }
+    }
 }
 
 
@@ -18,50 +29,24 @@ operator fun Pair<Int, Int>.plus(a: Pair<Int, Int>): Pair<Int, Int> {
     return first + a.first to second + a.second
 }
 
-enum class SideDir {
-
-    UP, LEFTUP, RIGHTUP, LEFTDOWN, RIGHTDOWN, DOWN, LEFT, RIGHT;
-
-
-    val vec: Pair<Int, Int>
-        get() {
-            return when (this) {
-                UP -> 1 to 0
-                DOWN -> -1 to 0
-                LEFT -> 0 to -1
-                RIGHT -> 0 to 1
-                LEFTUP -> LEFT.vec + UP.vec
-                RIGHTUP -> RIGHT.vec + UP.vec
-                LEFTDOWN -> LEFT.vec + DOWN.vec
-                RIGHTDOWN -> RIGHT.vec + DOWN.vec
-            }
-        }
-
-    fun rotated(): SideDir {
-        return when (this) {
-            UP -> RIGHTUP
-            DOWN -> LEFTDOWN
-            LEFT -> LEFTUP
-            RIGHT -> RIGHTDOWN
-            RIGHTUP -> RIGHT
-            RIGHTDOWN -> DOWN
-            LEFTDOWN -> LEFT
-            LEFTUP -> UP
-        }
-    }
-}
-
 data class FarmGroup(val kind: String, val plants: List<Pair<Int, Int>>) {
-    private fun getEdgePts(map: List<List<String>>) = plants.filter { (x, y) ->
-        FarmDir.entries.any { dir ->
+    private fun getEdges(map: List<List<String>>) = plants.flatMap { (x, y) ->
+        FarmDir.entries.filter { dir ->
             val (dx, dy) = dir.vec
             val (posX, posY) = x + dx to y + dy
             posX < 0 || posX >= map.size || posY < 0 || posY >= map[0].size || map[posX][posY] != kind
+        }.map { dir ->
+            when (dir) {
+                FarmDir.UP -> (x to y) to ((x to y) + FarmDir.RIGHT.vec)
+                FarmDir.DOWN -> ((x to y) + FarmDir.DOWN.vec + FarmDir.RIGHT.vec) to ((x to y) + FarmDir.DOWN.vec)
+                FarmDir.LEFT -> ((x to y) + FarmDir.DOWN.vec) to (x to y)
+                FarmDir.RIGHT -> ((x to y) + FarmDir.RIGHT.vec) to ((x to y) + FarmDir.RIGHT.vec + FarmDir.DOWN.vec)
+            }
         }
     }
 
     private fun calPerimeterOnMap(map: List<List<String>>): Int {
-        return getEdgePts(map).sumOf { (x, y) ->
+        return plants.sumOf { (x, y) ->
             FarmDir.entries.count { dir ->
                 val (dx, dy) = dir.vec
                 val (posX, posY) = x + dx to y + dy
@@ -71,43 +56,19 @@ data class FarmGroup(val kind: String, val plants: List<Pair<Int, Int>>) {
     }
 
     private fun calSidesOnMap(map: List<List<String>>): Int {
-        val pts = getEdgePts(map)
-        val sortedPts = pts.sortedBy { (x, y) -> y }.sortedBy { (x) -> x }
-
-        val visits = HashSet<Pair<Int, Int>>()
-        var (x, y) = pts.first()
-        var nextDir = SideDir.UP
-        visits.add(x to y)
-        while (visits.size != pts.size) {
-            val currentDir = nextDir
-            do {
-                val (dx, dy) = nextDir.vec
-                val pt = sortedPts.firstOrNull {
-                    !visits.contains(it) &&
-
-                            it == x + dx to y + dy
-                }
-                if (pt != null) {
-                    x = pt.first
-                    y = pt.second
-                    break
-                }
-                nextDir = nextDir.rotated()
-            } while (nextDir != currentDir)
-            visits.add(x to y)
+        val edges = getEdges(map).groupBy({ it.first }) { it.second }
+        return edges.entries.sumOf { (p1, v) ->
+            v.count { p2 ->
+                FarmDir.from(p1 to p2) != FarmDir.from(p2 to edges[p2]!!.first())
+            }
         }
-        return 0
     }
 
     fun calPriceOnMap(map: List<List<String>>) =
-        calPerimeterOnMap(map).also { kotlin.io.println("$kind Perimeter: $it") } * plants.size.also {
-            kotlin.io.println(
-                "Area: $it"
-            )
-        }
+        plants.size * calPerimeterOnMap(map)
 
     fun calPriceWithBulkDiscountOnMap(map: List<List<String>>) =
-        plants.size * calSidesOnMap(map).also { kotlin.io.println("$kind Sides: $it") }
+        plants.size * calSidesOnMap(map)
 }
 
 fun Array<Array<Boolean>>.nonVisitedPos(): Pair<Int, Int>? {
@@ -170,20 +131,20 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val map = input.map { it.map { it.toString() } }
-        return findGroups(map).also { println(it.size) }.sumOf { it.calPriceOnMap(map) }
+        return findGroups(map).sumOf { it.calPriceOnMap(map) }
     }
 
     fun part2(input: List<String>): Int {
         val map = input.map { it.map { it.toString() } }
-        return findGroups(map).also { println(it.size) }
+        return findGroups(map)
             .sumOf { it.calPriceWithBulkDiscountOnMap(map) }
     }
 
     val testInput = readInput("Day12_test")
     check(part1(testInput) == 1930)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 1206)
 
-//    val input = readInput("Day12")
-//    part1(input).println()
-//    part2(input).println()
+    val input = readInput("Day12")
+    part1(input).println()
+    part2(input).println()
 }
