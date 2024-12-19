ee221f5cb54c5f32a66a04bca74219082e54d6e5
mark wu <saybyebyet@gmail.com>
2024-12-19 23:48:43 +0800

    Day 16 - part 2

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,42 +1,102 @@
+data class Path(
+    val pos: Pair<Int, Int>,
+    val cost: Int,
+    val dir: Dir,
+    val points: Set<Pair<Pair<Int, Int>, Dir>>
+)
 
-
-
-fun findMinCostOnMaze(map: MutableList<MutableList<Char>>, visitedSet: Set<Pair<Int,Int>>, pos: Pair<Int, Int>, dir: Dir, acc: Int): Int {
-    return when(dir) {
-        Dir.UP, Dir.DOWN -> listOf(dir, Dir.LEFT, Dir.RIGHT)
-        Dir.LEFT, Dir.RIGHT-> listOf(dir, Dir.UP, Dir.DOWN)
-    }.minOf {
-        val (vx, vy) = pos + it.value
-        if (vx !in map[0].indices || vy !in map.indices || map[vy][vx] == '#' || map[vy][vx] == '@') {
-            Int.MAX_VALUE
-        } else if (map[vy][vx] == 'E') {
-            acc + 1
-        } else {
-            map[vy][vx] = '@'
-            val v = findMinCostOnMaze(map, visitedSet, vx to vy, it, acc + 1 + if (it == dir) 0 else 1000)
-            map[vy][vx] = '.'
-            v
+fun findMinCostOnMaze(map: List<String>): Pair<List<Path>, Map<Pair<Pair<Int, Int>, Dir>, List<Path>>> {
+    val start = 1 to map.size - 2
+    val startPath = Path(start, 0, Dir.RIGHT, setOf(start to Dir.RIGHT))
+    val queue = arrayListOf(startPath)
+    val finishedCandidates = ArrayList<Path>()
+    var minCost = Int.MAX_VALUE
+    val visitedSet = HashSet<Pair<Pair<Int, Int>, Dir>>()
+    val pathMap = HashMap<Pair<Pair<Int, Int>, Dir>, MutableList<Path>>()
+    while (queue.isNotEmpty()) {
+        val path = queue.minBy { it.cost }
+        queue.remove(path)
+        if (map[path.pos.second][path.pos.first] == 'E') {
+            finishedCandidates.add(path)
+            pathMap.getOrPut(path.pos to path.dir) { ArrayList() }.add(path)
+            if (path.cost <= minCost) {
+                minCost = path.cost
+            }
+            continue
         }
+        if (visitedSet.contains(path.pos to path.dir)) {
+            val diff = path.cost - pathMap[path.pos to path.dir]!!.first().cost
+            if (diff == 0) {
+                pathMap[path.pos to path.dir]!!.add(path)
+            }
+            continue
+        }
+        pathMap.getOrPut(path.pos to path.dir) { ArrayList() }.add(path)
+        visitedSet.add(path.pos to path.dir)
+        val nextQueue = when (path.dir) {
+            Dir.RIGHT, Dir.LEFT -> listOf(path.dir, Dir.UP, Dir.DOWN)
+            Dir.UP, Dir.DOWN -> listOf(path.dir, Dir.RIGHT, Dir.LEFT)
+        }.map {
+            val nextPos = path.pos + it.value
+            Path(
+                nextPos,
+                path.cost + 1 + if (path.dir == it) 0 else 1000,
+                it,
+                path.points + (nextPos to it)
+            )
+        }
+            .filter { (pos) -> pos.first in map[0].indices && pos.second in map.indices && map[pos.second][pos.first] != '#' }
+        queue.addAll(nextQueue)
     }
+    return finishedCandidates.filter { it.cost == minCost } to pathMap
 }
 
 fun main() {
 
     fun part1(input: List<String>): Int {
-        val map = input.map { it.toMutableList() }.toMutableList()
-        return findMinCostOnMaze(map, HashSet(), 1 to input.size - 2, Dir.RIGHT, 0).also { println(map.joinToString("\n") {it.joinToString("") } ) }
+        val (path, _) = findMinCostOnMaze(input)
+        return path.first().cost
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val (path, pathMap) = findMinCostOnMaze(input)
+        val endPath = path.first()
+
+        val branches = arrayListOf(endPath)
+        val pts = HashSet<Pair<Pair<Int, Int>, Dir>>()
+        while (branches.isNotEmpty()) {
+            val p = branches.removeFirst()
+            branches.addAll(p.points.filter { !pts.contains(it) }.map { pathMap[it]!! }
+                .filter { it.size > 1 }.flatten())
+            pts.addAll(p.points)
+        }
+        val sb = StringBuilder()
+        for (i in input.indices) {
+            if (i > 0) {
+                sb.append('\n')
+            }
+            for (j in input[i].indices) {
+                sb.append(
+                    if (pts.any { it.first == j to i }) {
+                        'O'
+                    } else if (input[i][j] == '#') {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                )
+            }
+        }
+        println(sb.toString())
+        return sb.count { it == 'O' }
     }
 
     val testInput = readInput("Day16_test")
-    check(part1(testInput).also { println(it) } == 11048)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 7036)
+    check(part2(testInput) == 45)
 
 
     val input = readInput("Day16")
-    part1(input).println()
+    check(part1(input) == 107468)
     part2(input).println()
 }
