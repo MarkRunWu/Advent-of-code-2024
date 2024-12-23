75de7ef033dbe9cac7765736babf934781f17a07
mark wu <saybyebyet@gmail.com>
2024-12-23 16:24:18 +0800

    Day 19 - part 2

--- src/Day19.kt
+++ src/Day19.kt
@@ -3,17 +3,19 @@ fun String.isPossible(colors: Set<String>, possibles: MutableMap<String, Boolean
         return possibles[this]!!
     }
     val possible = colors.asSequence().filter { startsWith(it) }.any {
-        if (it.length == length) true else substring(it.length).isPossible(colors, possibles)}
+        if (it.length == length) true else substring(it.length).isPossible(colors, possibles)
+    }
     possibles[this] = possible
     return possible
 }
 
-fun String.possibility(colors: Set<String>, possibles: MutableMap<String, Boolean>): Int {
+fun String.possibility(colors: Set<String>, possibles: MutableMap<String, Long>): Long {
     if (possibles.contains(this)) {
         return possibles[this]!!
     }
-    val possible = colors.asSequence().filter { startsWith(it) }.any {
-        if (it.length == length) true else substring(it.length).isPossible(colors, possibles)}
+    val possible = colors.asSequence().filter { startsWith(it) }.sumOf {
+        if (it.length == length) 1 else substring(it.length).possibility(colors, possibles)
+    }
     possibles[this] = possible
     return possible
 }
@@ -28,13 +30,17 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val colors = input.first().split(", ").toSet()
+        val designs = input.subList(2, input.size)
+        return designs.sumOf {
+            it.possibility(colors, HashMap())
+        }
     }
 
     val testInput = readInput("Day19_test")
     check(part1(testInput).also { println(it) } == 6)
-    check(part2(testInput) == 0)
+    check(part2(testInput).also { println(it) } == 16L)
 
     val input = readInput("Day19")
     part1(input).println()
