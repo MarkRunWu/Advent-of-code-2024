8fead0c2225b396a7d8e28bd277dc35db8f24c08
mark wu <saybyebyet@gmail.com>
2024-12-27 16:12:09 +0800

    Day 20: todo

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,20 +1,26 @@
-fun minBFS(map: List<String>, start: Pair<Int, Int>, end: Pair<Int, Int>): Int {
+fun minBFS(map: List<String>, start: Pair<Int, Int>, end: Pair<Int, Int>, maxCheatCount: Int = 0): Int {
     var picoSecs = 0
-    val queue = arrayListOf(start)
-    val visited = HashSet<Pair<Int, Int>>()
+    val startPos = start to 0
+    val queue = arrayListOf(startPos)
+    val visited = HashSet<Pair<Pair<Int, Int>,Int>>()
+    val costs = HashMap<Int, Int>()
     while (queue.isNotEmpty()) {
-        if (queue.contains(end)) {
-            return picoSecs
-        }
-        val nextQueue = ArrayList<Pair<Int, Int>>()
-        for (pos in queue) {
-            visited.add(pos)
+        val nextQueue = ArrayList<Pair<Pair<Int, Int>, Int>>()
+        for ((pos, cheatCount) in queue) {
+            if (pos == end) {
+                costs[cheatCount] = costs.getOrPut(cheatCount) { 0 } + 1
+            }
+            visited.add(pos to cheatCount)
             for ((posX, posY) in Dir.entries.map {
                     pos + it.value
             }) {
                 val nextPos = posX to posY
-                if (posX in map[0].indices && posY in map.indices && map[posY][posX] != '#' && !visited.contains(nextPos)) {
-                    nextQueue.add(nextPos)
+                if (posX in map[0].indices && posY in map.indices && !visited.contains(nextPos to cheatCount)) {
+                    if (map[posY][posX] != '#')  {
+                        nextQueue.add(nextPos to cheatCount)
+                    } else if (cheatCount < maxCheatCount) {
+                        nextQueue.add(nextPos to cheatCount+1)
+                    }
                 }
             }
         }
@@ -80,13 +86,18 @@ fun main() {
         return savingCostCount.also { println(it) }.filter { it.key >= minSavingPicoSec }.values.sum()
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun part2(input: List<String>): Int {
+        val start = input.getStartPosOrNone() ?: return -1
+        val end = input.getEndPosOrNone() ?: return -1
+        val before = minBFS(input, start, end)
+        val savingCostCount = HashMap<Int, Int>()
+
+        return savingCostCount.values.sum()
     }
 
     val testInput = readInput("Day20_test")
     check(part1(testInput, 64).also { println(it) } == 1)
-    check(part2(testInput) == 0L)
+    check(part2(testInput) == 0)
 
     val input = readInput("Day20")
     part1(input, 100).println()
