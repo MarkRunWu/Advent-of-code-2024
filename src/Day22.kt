29ab61830dccf2580940066c396de2fc205763ea
Mark Wu <saybyebyet@gmail.com>
2024-12-31 10:42:11 +0800

    Day 22 - part 2

--- src/Day22.kt
+++ src/Day22.kt
@@ -6,6 +6,20 @@ fun Long.next(): Long {
     return (step2 * 2048L).xor(step2) % 16777216L
 }
 
+data class Buyer(
+    val prices: List<Int>,
+) {
+    private val diff = prices.zipWithNext { l, r -> r - l }
+    val patterns = diff.run {
+        val r = HashMap<List<Int>, Int>()
+        for (i in 0..size - 4) {
+            val pattern = diff.subList(i, i + 4)
+            r.putIfAbsent(pattern, prices[i + 4])
+        }
+        r
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Long {
         return input.map {
@@ -20,12 +34,25 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val buyers = input.map {
+            it.toLong()
+        }.map { num ->
+            val r = ArrayList<Int>()
+            var current = num
+            repeat(2000) {
+                val price = (current % 10).toInt()
+                r.add(price)
+                current = current.next()
+            }
+            Buyer(prices = r)
+        }
+        return buyers.flatMap { it.patterns.entries }.groupBy { it.key }.mapValues { it.value.sumOf { v -> v.value } }
+            .maxOf { it.value }
     }
 
+    check(part1(listOf("1", "10", "100", "2024")).also { println(it) } == 37327623L)
     val testInput = readInput("Day22_test")
-    check(part1(testInput).also { println(it) } == 37327623L)
-    check(part2(testInput) == 0)
+    check(part2(testInput).also { println(it) } == 23)
 
     val input = readInput("Day22")
     part1(input).println()
